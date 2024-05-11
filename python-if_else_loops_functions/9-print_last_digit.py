78d4a89cff3eb074a767c0c5d75600fbbbc46df3
NicoBoni55 <bonifrank55@gmail.com>
2024-05-11 22:44:24 +0000

    pycode

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print((abs(number % 10)), end="")
-    return (abs(number))
+    new = (abs(number) % 10)
+    print((new), end="")
+    return (new)
