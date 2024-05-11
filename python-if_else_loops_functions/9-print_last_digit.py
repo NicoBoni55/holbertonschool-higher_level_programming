9aad2f24f408ec478e0c5f7996537c0d9a1ca7a5
NicoBoni55 <bonifrank55@gmail.com>
2024-05-11 22:06:05 +0000

    9-print_last

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    new_number = abs(number) % 10 
-    print ("{}".format(new_number))
+    new_number = abs(number) % 10
+    print("{}".format(new_number))
+    return(new_number)
