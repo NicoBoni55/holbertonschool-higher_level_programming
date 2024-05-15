86c1bd0412a2aab29bff10595a8bbc521699bb07
NicoBoni55 <bonifrank55@gmail.com>
2024-05-15 15:10:23 +0000

    pycode

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     resultado = []
     for numeros in my_list:
-        if my_list[numeros] % 2 == 0:
+        if numeros % 2 == 0:
             resultado.append(True)
         else:
             resultado.append(False)
