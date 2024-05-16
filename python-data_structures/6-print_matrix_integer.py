df943c1b93324ecfd98eeee9ccc0555851ba7484
NicoBoni55 <bonifrank55@gmail.com>
2024-05-16 16:40:45 +0000

    pycode

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,6 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         for y in x:
-            if (y != 3 or y != 6 or y != 9):
-                print("{:d}".format(y), end=" ")
+            print("{:d}".format(y), end=" ")
             if (y == 3 or y == 6 or y == 9):
-                print(' ')
+                print('')
