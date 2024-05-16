7ba2321830a6ea116329195ba60bbf12c0fbc6b8
NicoBoni55 <bonifrank55@gmail.com>
2024-05-16 18:08:03 +0000

    pycode

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for x in matrix:
-        i = 1
-        for y in x:
-            if (i == len(x)):
-                print("{:d}".format(y), end="\n")
-            else:
-                print("{:d}".format(y), end=" ")
-            i += 1
+    if matrix:
+        for x in matrix:
+            i = 1
+            for y in x:
+                if (i == len(x)):
+                    print("{:d}".format(y), end="\n")
+                else:
+                    print("{:d}".format(y), end=" ")
+                i += 1
