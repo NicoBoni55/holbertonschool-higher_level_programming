54081b5677685c62be86d992cfc0287b9f1ed263
NicoBoni55 <bonifrank55@gmail.com>
2024-05-18 21:19:01 +0000

    common_elements

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    for elementos1 in set_1:
+        for elementos2 in set_2:
+            if elementos1 == elementos2:
+                return set(elementos1)
