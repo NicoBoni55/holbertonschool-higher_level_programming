3286a4cff0c9d33bf01b6bc03d5e6fae8310329d
NicoBoni55 <bonifrank55@gmail.com>
2024-05-18 19:04:07 +0000

    pycode

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = []
-    if my_list:
-        for elemento in my_list:
-            if elemento == search:
-                new_list.append(replace)
-            else:
-                new_list.append(elemento)
-        return (new_list)
+    for elemento in my_list:
+        if elemento == search:
+            new_list.append(replace)
+        else:
+            new_list.append(elemento)
+    return (new_list)
