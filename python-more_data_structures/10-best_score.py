7c303508843657d1e03692726513f5bc5e086793
NicoBoni55 <bonifrank55@gmail.com>
2024-05-19 19:42:37 +0000

    pycode

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     else:
-        valor_max = max(a_dictionary.values())
+        valor_max = max(a_dictionary.values(), default=None)
         for llave, valor in a_dictionary.items():
             if valor == valor_max:
                 return llave
