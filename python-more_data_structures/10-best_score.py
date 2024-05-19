3d7cd5e372d6b7acd44f33d2619798c61117760c
NicoBoni55 <bonifrank55@gmail.com>
2024-05-19 19:29:43 +0000

    best

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        valor_max = max(a_dictionary.values())
+        for llave, valor in a_dictionary.items():
+            if valor == valor_max:
+                return llave
