7ed03baa9a15abd5d3661579caef47b2572f1d37
NicoBoni55 <bonifrank55@gmail.com>
2024-05-19 21:48:52 +0000

    pycode

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if type(value) is int:
-        try:
-            print("{:d}".format(value), end="")
-        except ValueError:
-            return False
-        print("")
-        return True
+    try:
+        print("{:d}".format(value), end="")
+    except ValueError:
+        return False
+    print("")
+    return True
