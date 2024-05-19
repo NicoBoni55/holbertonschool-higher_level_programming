2fbbe732183852bcca12f32e14a186e8719d9b72
NicoBoni55 <bonifrank55@gmail.com>
2024-05-19 21:34:56 +0000

    pycode

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for elementos in range(x):
         try:
             print("{:d}".format(my_list[elementos]), end="")
+            contador += 1
         except IndexError:
             break
-        contador += 1
     print("")
     return contador
