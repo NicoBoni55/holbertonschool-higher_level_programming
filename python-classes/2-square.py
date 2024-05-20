763d8d984557a9cfae00e49699d1d91253860383
NicoBoni55 <bonifrank55@gmail.com>
2024-05-20 01:46:08 +0000

    2-square

---
+++ python-classes/2-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Class Square
+
+"""
+
+
+class Square:
+    """
+    Esta clase representa un cuadrado
+
+     Atributos:
+     size
+
+    Métodos:
+    No tiene
+    """
+    def __init__(self, size=0):
+        """__init__
+
+        __init__ inicializa size
+
+        Atributos:
+        Size
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
