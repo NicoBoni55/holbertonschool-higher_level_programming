04aa3dcdb248698c9baaeaa9351dbcd6f758ca8a
NicoBoni55 <bonifrank55@gmail.com>
2024-05-20 02:39:25 +0000

    4-square

---
+++ python-classes/4-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Class Square
+
+"""
+
+
+class Square:
+    """
+    Esta clase representa un cuadrado
+
+     Atributos:
+     size
+
+    Métodos:
+    No tiene
+    """
+    def __init__(self, size=0):
+        """__init__
+
+        __init__ inicializa size
+
+        Atributos:
+        Size
+
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """area
+
+        area del cuadrado
+
+        Atributos:
+        self
+
+        """
+        return pow(self.__size, 2)
