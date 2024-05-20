6baca75dc95185996db68ff697f2eae9d4fe2c63
NicoBoni55 <bonifrank55@gmail.com>
2024-05-20 00:57:21 +0000

    1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Class Square
+
+"""
+
+
+class Square:
+    """
+    Esta clase representa un cuadrado
+
+     Atributos:
+    Size
+
+    Métodos:
+    No tiene
+    """
+    def __init__(self, size):
+        self.size = size
