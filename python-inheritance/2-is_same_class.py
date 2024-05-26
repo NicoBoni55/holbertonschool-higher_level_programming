3a063f4ce8234efdfabb521983ea0fdd5b9fcfdc
NicoBoni55 <bonifrank55@gmail.com>
2024-05-26 21:53:05 +0000

    is_same_class

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+same type class
+"""
+
+def is_same_class(obj, a_class):
+    """
+    if obj is exactly an instance of the class return true
+    in otherwise return false
+    """
+    if type(obj) != a_class:
+        return False
+    else:
+        return True
