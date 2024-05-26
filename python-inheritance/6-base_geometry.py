81b7da8647863d495f1b6ccc070b8123dbe25aac
NicoBoni55 <bonifrank55@gmail.com>
2024-05-26 22:53:28 +0000

    base_geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+base_geometry
+"""
+
+
+class BaseGeometry():
+    """
+    geometry class
+    """
+
+    def area(self):
+        """
+        define area
+        """
+        raise Exception("area() is not implemented")
