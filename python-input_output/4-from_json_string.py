2926cd48f3530f1354e3e38d6a2b4d376a168d4c
NicoBoni55 <bonifrank55@gmail.com>
2024-05-28 22:02:47 +0000

    from_json_string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+import json
+"""
+import json
+"""
+return an object
+"""
+
+
+def from_json_string(my_str):
+    """
+    return an object
+    """
+    return json.loads(my_str)
