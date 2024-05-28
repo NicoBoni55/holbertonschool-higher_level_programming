5ca716d32c10dd48efe1b892c8ac6aaf1cd77e2d
NicoBoni55 <bonifrank55@gmail.com>
2024-05-28 19:34:03 +0000

    pycode

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -9,5 +9,4 @@ def write_file(filename="", text=""):
     write file
     """
     with open(filename, encoding='UTF8') as f:
-        file = f.write(text)
-        return file
+        return len(f.readline())
