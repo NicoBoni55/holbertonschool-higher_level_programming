7d7d9befebf6b8221c7ea9bbfe1b034a4ac7bbab
NicoBoni55 <bonifrank55@gmail.com>
2024-05-28 19:29:19 +0000

    write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,10 +3,11 @@
 write file
 """
 
+
 def write_file(filename="", text=""):
     """
     write file
     """
     with open(filename, encoding='UTF8') as f:
         file = f.write(text)
-        print(len(file))
+        return file
