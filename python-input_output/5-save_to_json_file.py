09c0a6ac9589c43e65e74ea0f609be6a90caf9f4
NicoBoni55 <bonifrank55@gmail.com>
2024-05-29 13:11:15 +0000

    pycode

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,5 +9,8 @@ save to json file
 
 
 def save_to_json_file(my_obj, filename):
+    """
+    save_to_json_file
+    """
     with open(filename, 'w', encoding='UTF8') as f:
         return json.dump(my_obj, f)
