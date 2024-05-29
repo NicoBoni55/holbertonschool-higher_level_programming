71dba0abb4cc23580fb9f8e2be6c8af1a203a691
NicoBoni55 <bonifrank55@gmail.com>
2024-05-29 13:31:22 +0000

    load_from_json_file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+import json
+"""
+import json
+"""
+create an object from JSON
+"""
+
+
+def load_from_json_file(filename):
+    """
+    load_from_json_file
+    """
+    with open(filename, 'w', encoding='UTF8') as f:
+        return json.load(f)
