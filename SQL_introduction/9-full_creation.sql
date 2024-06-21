fe8edd6c168e386a63f6ac2ba9c34696902e466a
NicoBoni55 <bonifrank55@gmail.com>
2024-06-21 16:05:00 +0000

    full_creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,19 +2,24 @@
 -- con los datos id, name y score
 CREATE TABLE IF NOT EXISTS second_table
 (
-    id INT,
-    name VARCHAR(256),
-    score INT
+    id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    score INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 INSERT INTO second_table (id, name, score)
-VALUES (1, 'Jhon', 10);
+VALUES (1, 'Jhon', 10)
+ON DUPLICATE KEY UPDATE name=VALUES(name), score=VALUES(score);
 
 INSERT INTO second_table (id, name, score)
-VALUES (2, 'Alex', 3);
+VALUES (2, 'Alex', 3)
+ON DUPLICATE KEY UPDATE name=VALUES(name), score=VALUES(score);
 
 INSERT INTO second_table (id, name, score)
-VALUES (3, 'Bob', 14);
+VALUES (3, 'Bob', 14)
+ON DUPLICATE KEY UPDATE name=VALUES(name), score=VALUES(score);
 
 INSERT INTO second_table (id, name, score)
-VALUES (4, 'George', 8);
+VALUES (4, 'George', 8)
+ON DUPLICATE KEY UPDATE name=VALUES(name), score=VALUES(score);
