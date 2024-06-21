0f1442a3a032ed974ca9f281fd5e7d9eb4a2f3a4
NicoBoni55 <bonifrank55@gmail.com>
2024-06-21 00:36:32 +0000

    full_tables

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,8 +1,2 @@
 -- descripcion completa de una tabla sql
-SELECT 
-    *
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_NAME = 'first_table'
-    AND TABLE_SCHEMA = 'hbtn_0c_0';
+SHOW CREATE TABLE first_table;
