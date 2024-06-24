0ad0db56c4ef68b858101086c3cdb4f7f8ee7014
NicoBoni55 <bonifrank55@gmail.com>
2024-06-24 01:28:03 +0000

    privileges

--- 0-privileges.sql
+++ 0-privileges.sql
@@ -1,11 +1,3 @@
 -- listar todos los privilegios de los usuarios
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
-FLUSH PRIVILEGES;
-
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
