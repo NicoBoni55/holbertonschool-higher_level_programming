77dcb9aa2dec5954536a1494b79e620804ed7f9a
NicoBoni55 <bonifrank55@gmail.com>
2024-07-01 19:48:32 -0400

    model_state_insert

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+script that prints the State object with the name passed
+"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine(f'mysql+mysqldb://{mysql_username}:{mysql_password}@localhost:3306/{database_name}')
+
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    new_state = State(name = "Louisiana")
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+
+    session.close()
