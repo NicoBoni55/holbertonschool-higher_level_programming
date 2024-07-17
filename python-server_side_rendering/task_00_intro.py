409ed020883310324f842972a4299aa9d5222524
NicoBoni55 <bonifrank55@gmail.com>
2024-07-17 16:19:00 -0400

    task_00

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -14,24 +14,23 @@ def generate_invitations(template, attendees):
         raise ValueError("Template is empty, no output files generated.")
     if attendees is None:
         raise ValueError("No data provided, no output files generated.")
-    else:
         
-        expected_keys = ["name", "event_title", "event_date", "event_location"]
-        for elements in attendees:
-            for key in expected_keys:
-                if key not in elements or elements[key] is None:
-                    elements[key] = "N/A"
+    expected_keys = ["name", "event_title", "event_date", "event_location"]
+    for elements in attendees:
+        for key in expected_keys:
+            if key not in elements or elements[key] is None:
+                elements[key] = "N/A"
         
-        with open("template.txt", "r") as template_file:
-            template = template_file.read()
+    with open("template.txt", "r") as template_file:
+        template = template_file.read()
         
-        if not template.strip():
-            raise ValueError("Template file is empty, no output files generated.")
+    if not template.strip():
+        raise ValueError("Template file is empty, no output files generated.")
         
-        for X, elements in enumerate(attendees, start=1):
-            output = template.format(**elements)
+    for X, elements in enumerate(attendees, start=1):
+        output = template.format(**elements)
                 
-            output_numerate = "output_{}.txt".format(X)
+        output_numerate = "output_{}.txt".format(X)
 
-            with open(output_numerate, "w") as output_file:
-                output_file.write(output)
+        with open(output_numerate, "w") as output_file:
+            output_file.write(output)
