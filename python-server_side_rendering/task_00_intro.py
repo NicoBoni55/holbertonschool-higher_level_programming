0755cd34f18d91fab2c8524f1e22188aac544c74
NicoBoni55 <bonifrank55@gmail.com>
2024-07-17 14:02:26 -0400

    task_00

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -3,19 +3,30 @@ import os
 
 def generate_invitations(template, attendees):
 
-    if type(template) != str:
+    path = os.getcwd()
+    if os.path.exists(path) == False:
+        raise ValueError("path not exist")
+    if type(template) is not str:
         raise TypeError("template must be a string")
-    if type(attendees) != dict:
+    if type(attendees) is not list and type(attendees) is not dict:
         raise TypeError("attendees must be a dictionary")
-    if template is None:
-        raise ValueError("Don´t have a template")
-    if attendees is None:
-        raise ValueError("Don´t have a attendees")
+    if template is "":
+        raise ValueError("Template is empty, no output files generated.")
+    if attendees is []:
+        raise ValueError("No data provided, no output files generated.")
     else:
-        for element in attendees:
-            element_name = element.get("name")
-            element_event_title = element.get("event_title")
-            element_event_date = element.get("event_date")
-            element_event_location = element.get("event_location")    
+        for elements in attendees:
+            for key, value in elements.items():
+                if value is None:
+                    elements[key] = "N/A"
         
-        element_name.replace()
+        with open("template.txt", "r") as template_file:
+            template = template_file.read()
+        
+        for X, elements in enumerate(attendees, start=1):
+            output = template.format(**elements)
+                
+            output_numerate = "output{}.txt".format(X)
+
+            with open(output_numerate, "w") as output_file:
+                output_file.write(output)
