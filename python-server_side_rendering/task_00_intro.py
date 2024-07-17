3264a4e65175ed42cdbe96e82a9ee418d6b74971
NicoBoni55 <bonifrank55@gmail.com>
2024-07-17 14:04:18 -0400

    task_00

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -10,9 +10,9 @@ def generate_invitations(template, attendees):
         raise TypeError("template must be a string")
     if type(attendees) is not list and type(attendees) is not dict:
         raise TypeError("attendees must be a dictionary")
-    if template is "":
+    if template is None:
         raise ValueError("Template is empty, no output files generated.")
-    if attendees is []:
+    if attendees is None:
         raise ValueError("No data provided, no output files generated.")
     else:
         for elements in attendees:
