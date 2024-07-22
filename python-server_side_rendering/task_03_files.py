f1e99400e4fe819db62dc5c86ccc8f7a33b90260
NicoBoni55 <bonifrank55@gmail.com>
2024-07-22 12:10:17 -0400

    task03

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -60,8 +60,8 @@ def table_products():
     if product_id:
         try:
             product_id = int(product_id)
-        except:
-            return render_template("product_display.html", error="Product not found"), 400
+        except ValueError:
+            return render_template("product_display.html", error="Product ID not valid"), 400
         filtered_products = []
         for product in product_list:
             if product['id'] == product_id:
